eb1443213dd090ad69b579a587dcf1135773fb0d
Jung Seungwon <swjung1@seegene.com>
2022-06-02 13:09:47 +0900

    test

---
+++ public/script.js
@@ -0,0 +1,43 @@
+// Formantic UI
+$('.ui.accordion')
+  .accordion()
+;
+$('.ui.sticky')
+  .sticky({
+    context: '#context'
+  })
+;
+$('.tool-tip').popup();
+
+
+$('.ui.checkbox')
+  .checkbox()
+  ;
+
+  $('.ui.radio.checkbox')
+  .checkbox()
+;
+$('.ui.dropdown')
+  .dropdown()
+;
+
+
+// $('.ui.fluid.dropdown')
+//   .dropdown({
+//     allowAdditions: true
+//   })
+// ;
+
+// $('#search-select')
+//   .dropdown()
+// ;
+
+// $('#db-dropdown')
+//   .dropdown()
+// ;
+
+
+$('.myPopup')
+.popup()
+;
+  // Customize for WISDOM
